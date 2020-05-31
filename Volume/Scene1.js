422671751c84222948c0a74705b9107fad940470
James D.John <duonghht1419@gmail.com>
2020-05-31 23:35:18 +0700

    Thêm thành công chức năng click và animation #3

--- Volume/Scene1.js
+++ Volume/Scene1.js
@@ -1,5 +1,7 @@
 let constWeightBallGreen = 515;
 let ballNumber = 8;
+let numberGame = 2;
+let isStart = 0;
 
 
 class Scene1 extends Phaser.Scene{
@@ -12,54 +14,192 @@ class Scene1 extends Phaser.Scene{
     preload() {
         this.load.image('backGround', 'Assets/backGround.png')
         this.load.image('frameWork', 'Assets/frameWork.png')
-        this.load.image('button', 'Assets/1.png')
-        this.load.image('buttonPlay', 'Assets/2.png')
-        this.load.image('ballSmall', 'Assets/3.png')
-        this.load.image('ballBig', 'Assets/4.png')
-        this.load.image('bearSmall', 'Assets/5.png')
-        this.load.image('bearBig', 'Assets/6.png')
-        this.load.image('bearBlueSmall', 'Assets/7.png')
-        this.load.image('bearBlueBig', 'Assets/8.png')
-        this.load.image('gatoBig1', 'Assets/9.png')
-        this.load.image('gatoSmall', 'Assets/10.png')
-        this.load.image('gatoBig2', 'Assets/11.png')
-        this.load.image('fruitSmall', 'Assets/12.png')
-        this.load.image('fruitBig', 'Assets/13.png')
-        this.load.image('carSmall', 'Assets/14.png')
-        this.load.image('carBig', 'Assets/15.png')
+
         this.load.image('ballGreen', 'Assets/ballGreen.png')
         this.load.image('speaker', 'Assets/speaker.png')
-
 
         this.load.image('ballGroup1', 'Assets/1/ballGroup1.png')
         this.load.image('bearGroup1', 'Assets/1/bearGroup1.png')
         this.load.image('gatoGroup1', 'Assets/1/gatoGroup1.png')
 
-        this.load.image('ballGroup2', 'Assets/2/ballGroup2.png')
-        this.load.image('bearBlueGroup2', 'Assets/2/bearBuleGroup2.png')
-        this.load.image('gatoGroup2', 'Assets/2/gatoGroup2.png')
+        this.load.image('button1', 'Assets/button1.png')
+        this.load.image('button2', 'Assets/button2.png')
+        this.load.image('button3', 'Assets/button3.png')
+        this.load.image('frameButton', 'Assets/frameButton.png')
 
-        this.load.image('ballGroup3', 'Assets/3/ballGroup3.png')
-        this.load.image('bearGroup3', 'Assets/3/bearGroup3.png')
-        this.load.image('gatoGroup3', 'Assets/3/gatoGroup3.png')
+        this.load.image('StartSheet', 'Assets/StartSheet.png')
+        this.load.image('startButton1', 'Assets/startButton1.png')
+        this.load.image('startButton2', 'Assets/startButton2.png')
+
+        this.load.image('image1', 'Assets/image1.png')
     }
 
     //create game;
     create(){
-
         this.backGround = this.add.image(0, 0, 'backGround').setOrigin(0, 0)
         this.frameWork = this.add.image(250, 20, 'frameWork').setOrigin(0, 0)
-
-
-        this.ballGroup1 = this.add.image(250, 90, 'ballGroup1').setOrigin(0, 0)
-        this.bearGroup1 = this.add.image(250, 240, 'bearGroup1').setOrigin(0, 0)
-        this.gatoGroup1 = this.add.image(250, 390, 'gatoGroup1').setOrigin(0, 0)
 
         let arr = new Array("ballGreen");
         for(let i = 0; i < ballNumber; i++){
             arr[i] = this.add.image(constWeightBallGreen += 25, 38, "ballGreen").setOrigin(0, 0);
         }
 
+        this.scene1()
+    }
+
+    buttonGroup(a, b){
+        this.frameButton = this.add.sprite(a, b, 'frameButton')
+
+        this.button1 = this.add.sprite(a - 90, b + 15, 'button1').setInteractive()
+        this.button1.on('pointerup', () => {
+            this.handleCorrect(this.button1);
+        })
+
+        this.button2 = this.add.sprite(a + 90, b + 15, 'button2').setInteractive()
+        this.button2.on('pointerup', () => {
+            this.handleWrong(this.button2)
+        })
+
+        this.button3 = this.add.sprite(a, b + 15, 'button3').setInteractive()
+        this.button3.on('pointerup', () => { this.handleWrong(this.button3) })
+
+        this.input.on('gameobjectover', function (pointer, gameObject) {
+            gameObject.setTint(0xFFC400)
+        })
+
+        this.input.on('gameobjectout', function (pointer, gameObject) {
+            gameObject.clearTint()
+        })
+    }
+
+    startButton(){
+        this.StartSheet = this.add.image(250, 80, 'StartSheet').setOrigin(0, 0)
+
+        this.startButton1 = this.add.image(660, 200, 'startButton1').setOrigin(0, 0)
+        this.startButton1.setInteractive().on('pointerover', () => {
+            this.startButton1.destroy()
+
+            this.startButton2 = this.add.image(625, 175, 'startButton2').setOrigin(0, 0)
+            this.startButton2.setInteractive().on('pointerup', () => {
+                this.StartSheet.destroy()
+                this.startButton2.destroy()
+                isStart = 1
+                this.guide1()
+            })
+            this.startButton2.on('pointerout', () =>{
+                this.StartSheet.destroy()
+                this.startButton2.destroy()
+                this.startButton1.destroy()
+                this.startButton();
+            })
+        })
+
+    }
+
+    handleCorrect(button){
+        button.setTint(0x7BD400)
+        this.deleteImage()
+        numberGame--;
+
+        this.time.delayedCall(50, function wrong() {
+            button.x += 11.5
+            button.y = -button.x + 1155
+            this.time.delayedCall(50, function wrong() {
+                button.x += 11.5
+                button.y = -button.x + 1155
+                this.time.delayedCall(50, function wrong() {
+                    button.x += 11.5
+                    button.y = -button.x + 1155
+                    this.time.delayedCall(50, function wrong() {
+                        button.x += 11.5
+                        button.y = -button.x + 1155
+                        this.time.delayedCall(50, function wrong() {
+                            button.x += 11.5
+                            button.y = -button.x + 1155
+                            this.time.delayedCall(50, function wrong() {
+                                button.x += 11.5
+                                button.y = -button.x + 1155
+                                this.time.delayedCall(50, function wrong() {
+                                    button.x += 11.5
+                                    button.y = -button.x + 1155
+                                    this.time.delayedCall(50, function wrong() {
+                                        button.x += 11.5
+                                        button.y = -button.x + 1155
+                                        this.time.delayedCall(50, function wrong() {
+                                            button.x += 11.5
+                                            button.y = -button.x + 1155
+                                            this.time.delayedCall(50, function wrong() {
+                                                button.x += 11.5
+                                                button.y = -button.x + 1155
+                                                this.time.delayedCall(500, function wrong() {
+                                                    this.add.image(button.x - 20, button.y, 'image1')
+                                                    button.destroy()
+                                                    this.time.delayedCall(500, function wrong() {
+                                                        if(numberGame == 1){
+                                                            this.guide2()
+                                                        }
+                                                    }, [], this)
+                                                }, [], this)
+                                            }, [], this)
+                                        }, [], this)
+                                    }, [], this)
+                                }, [], this)
+                            }, [], this)
+                        }, [], this)
+                    }, [], this)
+                }, [], this)
+            }, [], this)
+        }, [], this)
+
+    }
+
+    animationButton(){
+
+    }
+
+    handleWrong(button){
+
+        button.setTint(0xFF283D)
+
+        this.time.delayedCall(500, function wrong() {
+            if(checkTint == 0){
+                button.setTint(0xFFC400);
+            }
+        }, [], this)
+
+    }
+
+    scene1(){
+        this.ballGroup1 = this.add.image(250, 90, 'ballGroup1').setOrigin(0, 0)
+        this.startButton()
+    }
+
+    guide1() {
+
+        this.time.delayedCall(500, function wrong() {
+            this.bearGroup1 = this.add.image(250, 240, 'bearGroup1').setOrigin(0, 0)
+            this.time.delayedCall(500, function wrong() {
+                this.buttonGroup(800,430)
+            }, [], this)
+        }, [], this)
+
+    }
+
+    guide2() {
+        this.time.delayedCall(500, function wrong() {
+            this.gatoGroup1 = this.add.image(250, 400, 'gatoGroup1').setOrigin(0, 0)
+            this.time.delayedCall(500, function gude2() {
+                this.buttonGroup(800,590)
+            }, [], this)
+        }, [], this)
+
+    }
+
+    deleteImage(){
+        this.frameButton.destroy();
+        //this.button1.destroy();
+        this.button2.destroy();
+        this.button3.destroy();
     }
 
     //Update;
